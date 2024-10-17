d0c4e966f00f079b6b4710b386b0bd21620c2d99
ismail <aloui.ismail.ai@gmail.com>
2024-10-18 00:15:28 +0200

    PRO08 add skills

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { SkillsComponent } from './skills/skills.component';
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'skills', component: SkillsComponent },
 ];
 
 @NgModule({
