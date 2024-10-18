6faf540ec715a21f4ff400172bac9e4aaf7e9fcc
ismail <aloui.ismail.ai@gmail.com>
2024-10-18 13:48:03 +0200

    PRO09 add form contact me

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { SkillsComponent } from './skills/skills.component';
+import { ContactComponent } from './contact/contact.component';
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'skills', component: SkillsComponent },
+  { path: 'contact', component: ContactComponent },
 ];
 
 @NgModule({
