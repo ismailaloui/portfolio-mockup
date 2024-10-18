10de9a36a6e77b56fdd83428c1d838c72e15f4c6
ismail <aloui.ismail.ai@gmail.com>
2024-10-18 18:28:39 +0200

    PRO10 fixe error language and add contact form

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -3,13 +3,22 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { TranslationLoaderService } from '../service/translation-loader.service';
 import { locale as english } from '../shared/en';
 import { locale as french } from '../shared/fr';
+import emailjs, { type EmailJSResponseStatus } from '@emailjs/browser';
+
 
 @Component({
   selector: 'app-contact',
+
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+  form = {
+    name: '',
+    email: '',
+    message: ''
+  };
+
 
   constructor(private _translationLoaderService: TranslationLoaderService) {
     this._translationLoaderService.loadTranslations(english, french);
@@ -18,5 +27,24 @@ export class ContactComponent implements OnInit {
 
   ngOnInit(): void {
 
+  }
+
+
+
+  public sendEmail() {
+
+
+    emailjs
+      .send('service_96mo0db', 'template_lfn1b4f',this.form, {
+        publicKey: 'YSjkRgYmr9cDpyON1'
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', (error as EmailJSResponseStatus).text);
+        },
+      );
   }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { TranslationLoaderService } from '../service/translation-loader.service';
+import { locale as english } from '../shared/en';
+import { locale as french } from '../shared/fr';
+import { TranslateService } from '@ngx-translate/core';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +11,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  constructor(private _translationLoaderService: TranslationLoaderService) {
+    this._translationLoaderService.loadTranslations(english, french);
+  }
+
 
 }
